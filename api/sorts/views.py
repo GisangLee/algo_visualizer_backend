10fce2e095ff6f3340c59645fe3f5aa49e7e99d0
dude <masterkorea01@naver.com>
2022-08-30 15:20:13 +0900

    feat ( selection sorting) :
    Done : 선택 정렬

--- api/sorts/views.py
+++ api/sorts/views.py
@@ -46,6 +46,58 @@ class SortViewSet(mixins.BaseModelViewSet):
 
         return result
 
+    def __insertion_sort(self, data):
+
+        result = []
+
+        initial_data = copy.deepcopy(data)
+
+        result.append(initial_data)
+
+        for idx in range(1, len(data)):
+
+            j = idx
+
+            while j > 0 and data[j - 1] > data[j]:
+                data[j - 1], data[j] = data[j], data[j - 1]
+                tmp = copy.deepcopy(data)
+                result.append(tmp)
+                j -= 1
+
+            # for idx in range(a, 0, -1):
+
+            #     if data[idx] < data[idx - 1]:
+            #         data[idx], data[idx - 1] = data[idx - 1], data[idx]
+            #         tmp = copy.deepcopy(data)
+            #         result.append(tmp)
+
+            #     else:
+            #         break
+
+        return result
+
+    def __selection_sort(self, data):
+
+        initial_data = copy.deepcopy(data)
+        result = [initial_data]
+
+        loop = len(data)
+
+        for i in range(loop):
+
+            min_idx = i
+            for j in range(i+1, loop):
+
+                if (data[min_idx] > data[j]):
+                    min_idx = j
+
+            data[i], data[min_idx] = data[min_idx], data[i]
+
+            tmp = copy.deepcopy(data)
+            result.append(tmp)
+
+        return result
+
     @swagger_auto_schema(manual_parameters=bubble_sorts_doc.bubble_sorts_list, tags=["정렬 알고리즘"], operation_description="bubble, selection, insertionm merge, quick")
     def list(self, request, *args, **kwargs):
 
@@ -54,11 +106,6 @@ class SortViewSet(mixins.BaseModelViewSet):
         if sort_type is None:
 
             return Response(Error.error("정렬 타입을 지정해주세요."), status=status.HTTP_400_BAD_REQUEST)
-
-        max_arr_size = int(request.GET.get("max_size", None))
-
-        if max_arr_size is None:
-            return Response(Error.error("데이터 크기를 정해주세요."), status=status.HTTP_400_BAD_REQUEST)
 
         data = request.GET.get("data", None)
 
@@ -69,9 +116,18 @@ class SortViewSet(mixins.BaseModelViewSet):
 
         if sort_type == "bubble":
 
-            #random_data = self.__make_random_array(max_size=max_arr_size)
-
+            # random_data = self.__make_random_array(max_size=max_arr_size)s
             sorted_list = self.__bubble_sort(data)
             return Response(Success.response(self.__class__.__name__, request.method, sorted_list, "200"), status=status.HTTP_200_OK)
+
+        elif sort_type == "insertion":
+
+            sorted_list = self.__insertion_sort(data)
+            return Response(Success.response(self.__class__.__name__, request.method, sorted_list, "200"), status=status.HTTP_200_OK)
+
+        elif sort_type == "selection":
+            sorted_list = self.__selection_sort(data)
+            return Response(Success.response(self.__class__.__name__, request.method, sorted_list, "200"), status=status.HTTP_200_OK)
+
         else:
             return Response(Success.response(self.__class__.__name__, request.method, "만드는 중", "200"), status=status.HTTP_200_OK)
