7547b598c1b61b27bec9b8ef9b4c8662f403ca0a
dude <masterkorea01@naver.com>
2022-09-04 00:02:37 +0900

    d

--- config/settings/common.py
+++ config/settings/common.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = os.path.join(PROJ_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 
 # CORS_ORIGIN_WHITELIST = [
